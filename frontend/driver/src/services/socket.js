2941f49c3b48c9036d7de21a5e3076a5992406ac
indramohankumar <deviavlisha@gmail.com>
2026-07-30 19:10:16 +0530

    Force WebSockets for Render compatibility

--- frontend/driver/src/services/socket.js
+++ frontend/driver/src/services/socket.js
@@ -2,6 +2,7 @@ import { io } from 'socket.io-client';
 
 const socket = io('https://uber-clone-l9xh.onrender.com', {
     autoConnect: false,
+    transports: ['websocket']
 });
 
 export default socket;
--- frontend/rider/src/services/socket.js
+++ frontend/rider/src/services/socket.js
@@ -1,5 +1,6 @@
 import {io} from "socket.io-client";
 const socket=io("https://uber-clone-l9xh.onrender.com",{
     autoConnect:false,
+    transports: ['websocket']
 });
 export default socket;
